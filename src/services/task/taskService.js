9e50ce5155b397873040dea3573e64a617863c7d
LasandiRandini <randini-im20057@stu.kln.ac.lk>
2025-02-19 02:21:08 +0530

    updared

--- src/services/task/taskService.js
+++ src/services/task/taskService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getUserData } from "../auth/authService";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL; 
 const TASK_URL = `${BASE_URL}/task`;
@@ -6,10 +7,11 @@ const TASK_URL = `${BASE_URL}/task`;
 
 export const Create_Task = async (filteredParams) => {
   try {
+    const user = await getUserData();
     const taskData = {
       Template_Task_Id: 24,
       task_type: "Create Collect Only CPE List for Download",
-      Created_By: "FrontendUser", 
+      Created_By: user.user_id, 
       task_status: "open",
       ...filteredParams,
     };
@@ -34,10 +36,13 @@ export const Task_for_Download_Incidents = async (incidentData) => {
 
   export const Create_Task_for_OpenNoAgent = async (filteredParams) => {
     try {
+      const user = await getUserData();
       const taskData = {
+       
+        
         Template_Task_Id: 21,
         task_type: "Create incident open for distribution download",
-        Created_By: "FrontendUser", 
+        Created_By: user.user_id,  
         task_status: "open",
         Incident_Status: "Open No Agent",
         proceed_Dtm: "null",
@@ -54,10 +59,11 @@ export const Task_for_Download_Incidents = async (incidentData) => {
 
   export const Create_Task_for_Create_CaseFromIncident = async (filteredParams) => {
     try {
+      const user = await getUserData();
       const taskData = {
         Template_Task_Id: 15,
         task_type: "Create Case from Incident Direct NO Agent",
-        Created_By: "FrontendUser",
+        Created_By: user.user_id,  
         task_status: "open",
         Incident_Status: "Open No Agent",
         ...filteredParams,
@@ -73,10 +79,11 @@ export const Task_for_Download_Incidents = async (incidentData) => {
   
   export const Create_Task_for_Forward_CPECollect = async (filteredParams) => {
     try {
+    const user = await getUserData();
       const taskData = {
         Template_Task_Id: 17,
         task_type: "Create Case from Incident Open CPE Collect",
-        Created_By: "FrontendUser",
+        Created_By: user.user_id,  
         task_status: "open",
         Incident_Status: "Open CPE Collect",
         ...filteredParams,
@@ -92,10 +99,13 @@ export const Task_for_Download_Incidents = async (incidentData) => {
   
   export const Open_Task_Count_Forward_CPE_Collect = async () => { 
     try {
+      const user = await getUserData();
         const taskData = {
             Template_Task_Id: 17, 
             task_type: "Create Case from Incident Open CPE Collect",
+            Created_By: user.user_id,  
         };
+          
         const response = await axios.post(`${TASK_URL}/Open_Task_Count`, taskData);
         return response.data.openTaskCount; 
     } catch (error) {
@@ -106,8 +116,10 @@ export const Task_for_Download_Incidents = async (incidentData) => {
 
   export const Open_Task_Count_Incident_To_Case = async () => { 
     try {
+      const user = await getUserData();
         const taskData = {
             Template_Task_Id: 15, 
+            Created_By: user.user_id,  
             task_type: "Create Case from Incident Direct NO Agent",
         };
         const response = await axios.post(`${TASK_URL}/Open_Task_Count`, taskData);
