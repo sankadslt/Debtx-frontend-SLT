abc2812315f2110a1c263a3832b3491cd73fc0de
Warnakulaisuru <122584532+Warnakulaisuru@users.noreply.github.com>
2025-06-08 02:06:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./App.css";
-import Layout from "./components/Layout";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import "./App.css";
 
-function App() {
-  return (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
     <BrowserRouter basename="/slt">
       <Layout />
     </BrowserRouter>
-  );
-}
-
-export default App;
+  </React.StrictMode>
+);
