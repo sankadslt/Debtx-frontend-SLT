217bb3004917d2a98ee02cf80457740aba6a1325
Lishan Weerasuriya <lishanweerasuriya@gmail.com>
2025-06-08 04:18:57 +0530

    Updated Router for User Approval

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -109,7 +109,10 @@ import RtomInfoEdit from "../pages/Rtom/RtomInfoEdit";
 import RtomInfoEnd from "../pages/Rtom/RtomInfoEnd";
 import ListofServicetype from "../pages/Rtom/ListofServicetype";
 
-Route
+//USER  
+import UserList from "../pages/User/UserList";
+import UserInfo  from "../pages/User/UserInfo";
+import UserApproval from "../pages/User/UserApproval";
 
 
 
@@ -711,6 +714,14 @@ const Routers = () => {
     <Route path="/pages/DRC/DRCList" element={<ProtectedRoute element={<DRCList />} allowedRoles={['superadmin']} />} />
     <Route path="/pages/DRC/DRCInfo" element={<ProtectedRoute element={<DRCInfo />} allowedRoles={['superadmin']} />} />
     <Route path="/pages/DRC/DRCInfoEdit" element={<DRCInfoEdit />} /> */}
+
+    {/* User */}
+      <Route path="/pages/User/UserList" element={<ProtectedRoute element={<UserList />} allowedRoles={['superadmin']} />} />
+      <Route path="/pages/User/UserInfo" element={<ProtectedRoute element={<UserInfo />} allowedRoles={['superadmin']} />} />
+      <Route path="/pages/User/UserApproval" element={<ProtectedRoute element={<UserApproval />} allowedRoles={['superadmin']} />} />
+      
+      {/* RTOM */}
+
     </Routes>
 
     
