dd221240c79d917328e329a3cafc0c77668a5c74
Warnakulaisuru <122584532+Warnakulaisuru@users.noreply.github.com>
2025-06-27 13:24:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -377,18 +377,18 @@ const Navbar = () => {
     name: "Guest",
     role: "Visitor",
     profileOptions: [
-      {
-        id: 1,
-        label: "Profile",
-        icon: <FaUser />,
-        onClick: () => alert("Profile Clicked"),
-      },
-      {
-        id: 2,
-        label: "Settings & Privacy",
-        icon: <FaCog />,
-        onClick: () => alert("Settings Clicked"),
-      },
+      // {
+      //   id: 1,
+      //   label: "Profile",
+      //   icon: <FaUser />,
+      //   onClick: () => alert("Profile Clicked"),
+      // },
+      // {
+      //   id: 2,
+      //   label: "Settings & Privacy",
+      //   icon: <FaCog />,
+      //   onClick: () => alert("Settings Clicked"),
+      // },
       {
         id: 3,
         label: "Log Out",
@@ -624,7 +624,7 @@ const Navbar = () => {
 
         </div>
 
-        <FaCog className="w-6 h-6 text-blue-500 bg-white rounded-full p-1 shadow-md cursor-pointer" />
+       // <FaCog className="w-6 h-6 text-blue-500 bg-white rounded-full p-1 shadow-md cursor-pointer" />
         <div className="relative" ref={dropdownRef}>
           <div
             className="w-10 h-10 rounded-full overflow-hidden border-2 border-white cursor-pointer"
